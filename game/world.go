c5e9ef4c3a4ac066a4f6906f27ce0ec586382475
tauraamui <adamplewis@pm.me>
2019-12-16 13:50:49 +0000

    Add camera based transformation to world rendering scale and pos

--- game/world.go
+++ game/world.go
@@ -9,18 +9,19 @@ import (
 
 const (
 	TILE_SIZE         = 32
-	SCALE             = 3
+	SCALE             = 2
 	GRASS_TILES_START = 9
 	GRASS_TILES_END   = 11
 )
 
 func computeTilesToRender(rec pixel.Rect) (int, int) {
-	return int(rec.W() / (32 * SCALE)), int(rec.H() / (32 * SCALE))
+	return int(rec.W() / 32), int(rec.H() / 32)
 }
 
 func NewWorld(s pixel.Picture, tt []pixel.Rect) *World {
 	world := &World{
 		spriteSheet: s,
+		camPos:      pixel.ZV,
 	}
 
 	for i := GRASS_TILES_START; i < GRASS_TILES_END; i++ {
@@ -33,15 +34,18 @@ func NewWorld(s pixel.Picture, tt []pixel.Rect) *World {
 type World struct {
 	spriteSheet pixel.Picture
 	grassTiles  []*pixel.Sprite
+	camPos      pixel.Vec
 }
 
 func (w *World) Draw(win *pixelgl.Window) {
+	cam := pixel.IM.Scaled(w.camPos, SCALE).Moved(win.Bounds().Center().Sub(w.camPos))
+	win.SetMatrix(cam)
 	maxX, maxY := computeTilesToRender(win.Bounds())
 	for x := 0; x < maxX+5; x++ {
 		for y := 0; y < maxY+5; y++ {
 			rand.Seed(int64((x * 83) + (y * 385)))
 			grass := w.grassTiles[ranRange(0, 2)]
-			grass.Draw(win, pixel.IM.Scaled(pixel.ZV, SCALE).Moved(pixel.V(float64(x*(32*SCALE)), float64(y*(32*SCALE)))))
+			grass.Draw(win, pixel.IM.Moved(pixel.V(float64(x*(32)), float64(y*(32)))))
 		}
 	}
 }
