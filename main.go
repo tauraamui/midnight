b03aec32b7611056a832541ec336069b2bef9517
tauraamui <adamplewis@pm.me>
2019-12-26 18:25:18 +0000

    Add a night/dark mode shader which just darkens each pixel

--- main.go
+++ main.go
@@ -61,17 +61,12 @@ out vec4 fragColor;
 uniform vec4 uTexBounds;
 uniform sampler2D uTexture;
 
+float ambientStrength = 0.225f;
+vec3 lightColor = vec3(1.0, 1.0, 1.0);
+
 void main() {
 	// Get our current screen coordinate
 	vec2 t = (vTexCoords - uTexBounds.xy) / uTexBounds.zw;
-
-	// Sum our 3 color channels
-	float sum  = texture(uTexture, t).r;
-	      sum += texture(uTexture, t).g;
-	      sum += texture(uTexture, t).b;
-
-	// Divide by 3, and set the output to the result
-	vec4 color = vec4( sum/3, sum/3, sum/3, 1.0);
-	fragColor = color;
+	fragColor = vec4(((ambientStrength * lightColor) * texture(uTexture, t).rgb), 1.0);
 }
 `
