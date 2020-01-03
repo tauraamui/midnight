6ec07056e292c2445f420b43eacaba1e12c9d1d3
tauraamui <adamplewis@pm.me>
2020-01-03 23:00:25 +0000

    Fix ambientness to take over wherever spot light is too low

--- assets/shader/nighttime.glsl
+++ assets/shader/nighttime.glsl
@@ -10,27 +10,15 @@ uniform sampler2D uTexture;
 
 uniform float ambientLightIntensity;
 
-vec3 lightColor = vec3(0.9373, 0.7647, 0.4667);
 vec3 ambientLightColor = vec3(1.0);
+vec3 spotLightColor = vec3(0.8431, 0.0588, 0.0588);
 
 void main() {
 	// Get our current screen coordinate
 	vec2 t = (vTexCoords - uTexBounds.xy) / uTexBounds.zw;
-
 	vec3 tColor = vec3(texture(uTexture, t).rgb);
+	float distanceFromLight = 1-(distance(t, vec2(0.85)));
 	vec3 ambColor = (ambientLightIntensity * ambientLightColor) * tColor;
-	vec3 spotlightColor = vec3(0.0);
-
-	if (ambientLightIntensity < 0.9) {
-		float pct = 0.0;
-		float radius = 0.9;
-		pct = distance(t, vec2(0.55));
-		pct = smoothstep(radius-(radius*0.9), radius+(radius*0.9), dot(pct, pct)*4.0);
-		spotlightColor = vec3(((1 - pct) * ambientLightColor * lightColor) * tColor);
-		if (spotlightColor.x > ambColor.x && spotlightColor.y > ambColor.y && spotlightColor.z > ambColor.z) {
-			ambColor = spotlightColor;
-		}
-	}
-
+	ambColor = (ambientLightIntensity + (distanceFromLight * spotLightColor)) * tColor;
 	fragColor = vec4(ambColor, 1.0);
 }
