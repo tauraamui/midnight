b4156538ec93137a036bb4b4d6574da08187f293
tauraamui <adamplewis@pm.me>
2020-01-04 11:54:45 +0000

    Fiddle with spotlight attenuation until happy with light strength

--- assets/shader/nighttime.glsl
+++ assets/shader/nighttime.glsl
@@ -10,17 +10,23 @@ uniform sampler2D uTexture;
 
 uniform float ambientLightIntensity;
 
+float attenConst = 1.0;
+float attenLinear = 50.4;
+float attenQuadratic = 129.6;
+
 vec3 ambientLightColor = vec3(1.0, 1.0, 1.0);
-vec3 spotLightColor = vec3(0.8431, 0.0588, 0.0588);
+vec3 spotLightColor = vec3(0.7137, 0.8431, 0.0588);
 
 void main() {
 	// Get our current screen coordinate
 	vec2 t = (vTexCoords - uTexBounds.xy) / uTexBounds.zw;
 	vec3 tColor = texture(uTexture, t).rgb;
-	float distanceFromLight = 1-(distance(t, vec2(0.85)));
+	float distanceFromLight = distance(t, vec2(0.85));
+
+	float attenutation = 1.0 / (attenConst + (attenLinear * distanceFromLight) + (attenQuadratic * pow(distanceFromLight, 2)));
 
 	vec3 ambientLight = (ambientLightIntensity * ambientLightColor);
-	vec3 pointlightLight = (distanceFromLight * spotLightColor);
+	vec3 pointlightLight = (attenutation * spotLightColor);
 
 	vec3 ambColor = (ambientLight + pointlightLight) * tColor;
 	fragColor = vec4(ambColor, 1.0);
