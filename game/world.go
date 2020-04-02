9613ae4417f47858ad687a15aee141373b7698d4
tauraamui <adamplewis@pm.me>
2020-04-02 13:50:33 +0100

    Make world canvas copy to paint lights onto

--- game/world.go
+++ game/world.go
@@ -126,12 +126,17 @@ func (w *World) Draw(
 		w.movingL, w.movingR, w.movingU, w.movingD,
 	)
 
-	lights.Clear(pixel.RGB(0, 0, 0).Mul(pixel.Alpha(1)))
+	win.Draw(w.worldCopyCanvas, pixel.IM.Moved(w.worldCopyCanvas.Bounds().Center()))
+
+	lights.Clear(pixel.RGB(0, 0, 0).Mul(pixel.Alpha(0.85)))
 	for _, f := range w.fireflies {
 		lights.SetMatrix(pixel.IM.Moved(w.Camera.Project(pixel.V(float64(f.Pos().X()), float64(f.Pos().Y())))))
 		f.Draw(lights)
 	}
-	lights.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+	lights.SetComposeMethod(pixel.ComposePlus)
+	lights.Draw(w.worldCopyCanvas, pixel.IM.Moved(w.worldCopyCanvas.Bounds().Center()))
+
+	w.worldCopyCanvas.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
 
 	dbg.SetMatrix(w.Camera)
 	dbg.Color = pixel.RGB(1, 0, 0)
